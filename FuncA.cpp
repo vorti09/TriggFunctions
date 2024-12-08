e4a6c4d059af84f3d04e75e39aa7588f3d568630
vorti09 <ivannedorizanyuk@gmail.com>
2024-12-08 22:17:28 +0200

    Hotfix: calculate function sums first 3 terms

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,10 @@
-#include "FuncA.h"
+#include <cmath>
 
 double FuncA::calculate(double x) {
-    return 0; // Початковий етап
+    double result = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        double term = std::pow(x, 2 * i + 1) / std::tgamma(2 * i + 2);
+        result += term;
+    }
+    return result;
 }
