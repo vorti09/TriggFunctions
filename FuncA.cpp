ef1fe6bea5b27cbdf1be8affe078848ca9f6a718
vorti09 <ivannedorizanyuk@gmail.com>
2024-12-08 22:22:36 +0200

    Implemented FuncA with parameter n and added comments

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,16 @@
-#include "FuncA.h"
+#include <cmath>
 
-double FuncA::calculate(double x) {
-    return 0; // Початковий етап
+/**
+ * @brief Обчислює гіперболічний синус через суму перших n елементів ряду.
+ * @param x Аргумент функції.
+ * @param n Кількість елементів ряду, які будуть використані.
+ * @return Значення гіперболічного синуса.
+ */
+double FuncA::calculate(double x, int n) {
+    double result = 0.0;
+    for (int i = 0; i < n; ++i) {
+        double term = std::pow(x, 2 * i + 1) / std::tgamma(2 * i + 2); // Розклад ряду
+        result += term;
+    }
+    return result;
 }
